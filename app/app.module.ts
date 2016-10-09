c8baeef439d258b2e64e316f975c343bf602f6df
monsun <3Hubiny12>
2016-10-09 16:35:31 +0200

    toh-pt6

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,18 @@
+import './rxjs-extensions';
+
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms'
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }   from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./heroes.component";
 import { HeroDetailComponent } from "./hero-detail.component";
+import { HeroSearchComponent } from "./hero-search.component";
 import { DashboardComponent } from "./dashboard.component";
 import { HeroService } from "./hero.service";
 
@@ -14,13 +22,16 @@ import { routing } from "./app.routing";
   imports: [
       BrowserModule,
       FormsModule,
+      HttpModule,
+      InMemoryWebApiModule.forRoot(InMemoryDataService),
       routing
   ],
   declarations: [
       AppComponent,
       HeroesComponent,
       DashboardComponent,
-      HeroDetailComponent
+      HeroDetailComponent,
+      HeroSearchComponent
   ],
   providers: [
       HeroService
